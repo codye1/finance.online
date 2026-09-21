6d084f725af2abf30d66fa098addf10d69c8b847
codye1 <123339584+codye1@users.noreply.github.com>
2026-09-21 11:27:27 +0300

    #FO-4 connected db

---
+++ finance.online.api/Data/FinanceOnlineDbContext.cs
@@ -0,0 +1,16 @@
+﻿using finance.online.api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace finance.online.api.Data
+{
+    public class FinanceOnlineDbContext
+        : IdentityDbContext<AppUser>
+    {
+        public FinanceOnlineDbContext(
+            DbContextOptions<FinanceOnlineDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ finance.online.api/Models/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace finance.online.api.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string FullName { get; set; } = "";
+        public DateTime CreatedAt { get; set; }
+    }
+}
