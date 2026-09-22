3cb82e48f060b97e0cd6f567e80758e0f6e5d838
codye1 <123339584+codye1@users.noreply.github.com>
2026-09-22 08:35:15 +0300

    init mvc

---
+++ finance.online.mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace finance.online.mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
